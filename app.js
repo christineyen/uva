07e9f1c7a5909fdb91c026752d69cf2afe421caf
Christine Yen <cyen@christineyen.com>
2011-06-25 17:53:27 -0700

    Point to / use process.PORT for heroku

--- app.js
+++ app.js
@@ -29,5 +29,7 @@ app.get('/', function(req, res){
   });
 });
 
-app.listen();
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Express server listening on port %d in %s mode", port, app.settings.env);
+});
