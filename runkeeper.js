afd9a44db3d8a8fb38061f63e5e755ecb0f2a90e
Christine Yen <cyen@christineyen.com>
2011-10-27 13:11:15 -0700

    Lots of whitespace tweaks for runkeeper.js

--- runkeeper.js
+++ runkeeper.js
@@ -27,7 +27,7 @@ var request = require('request'),
       };
 
 var HealthGraph = exports.HealthGraph = function(options) {
-    
+
     this.client_id = options.client_id || null ;
     this.client_secret = options.client_secret || null;
     this.auth_url = options.auth_url || "https://runkeeper.com/apps/authorize";
@@ -47,78 +47,61 @@ var HealthGraph = exports.HealthGraph = function(options) {
 // getToken performs Step 2
 
 HealthGraph.prototype.getNewToken = function (authorization_code, callback) {
-
     var request_params = {
-	grant_type: "authorization_code",
-	code: authorization_code,
-	client_id: this.client_id,
-	client_secret: this.client_secret,
-	redirect_uri: this.redirect_uri
+      grant_type: "authorization_code",
+      code: authorization_code,
+      client_id: this.client_id,
+      client_secret: this.client_secret,
+      redirect_uri: this.redirect_uri
     };
-    
-    var paramlist  = [];
+
+    var paramlist = [];
     for (pk in request_params) {
-	paramlist.push(pk + "=" + request_params[pk]);
+      paramlist.push(pk + "=" + request_params[pk]);
     };
     var body_string = paramlist.join("&");
-    
-    var request_details = {  
-	method: "POST",
-	headers: {'content-type' : 'application/x-www-form-urlencoded'},
-	uri: this.access_token_url,
-	body: body_string
+
+    var request_details = {
+      method: "POST",
+      headers: {'content-type' : 'application/x-www-form-urlencoded'},
+      uri: this.access_token_url,
+      body: body_string
     };
-    
+
     console.log(request_details['method'] + " " + request_details['uri'] + "\n body: \n" + body_string);
-    
+
     request(request_details,
-	function(error, response, body) { 
-	    console.log(request_details['method'] + " " + request_details['uri'] + "\n");
-	    console.log("ERROR\n" + error);
-	    console.log("RESPONSE\n" + response);
-	    console.log("BODY\n" + body);
-	    callback(JSON.parse(body)['access_token']);
-        });
+      function(error, response, body) {
+        console.log(request_details['method'] + " " + request_details['uri'] + "\n");
+        console.log("ERROR\n" + error);
+        console.log("RESPONSE\n" + response);
+        console.log("BODY\n" + body);
+        callback(JSON.parse(body)['access_token']);
+      });
 };
- 
-/*
-var funcBuilder = function(func_name) {
-    return function(callback) {
-	console.log("This method is -- " + func_name);
-    }; 
-};
-*/
 
 for (func_name in API) {
-
     console.log("generating function HealthGraph." + func_name);
 
     HealthGraph.prototype[func_name] = (function(func_name) {
-	    return function(callback) {
-		console.log("This method is -- " + func_name);
-		var request_details = {
-		    method: API[func_name]['method'] || 'GET',
-		    headers: {'Accept': API[func_name]['media_type'],
-			      'Authorization' : 'Bearer ' + this.access_token},
-		    uri: "https://" + this.api_domain + API[func_name]['uri']
-		};
+      return function(callback) {
+        console.log("This method is -- " + func_name);
+        var request_details = {
+            method: API[func_name]['method'] || 'GET',
+            headers: {'Accept': API[func_name]['media_type'],
+                'Authorization' : 'Bearer ' + this.access_token},
+            uri: "https://" + this.api_domain + API[func_name]['uri']
+        };
 
-		console.log(request_details);
-
-		request(request_details,
-			function(error, response, body) { 
-			    console.log(request_details['method'] + " " + request_details['uri'] + "\n");
-			    console.log("ERROR\n" + error);
-			    console.log("RESPONSE\n" + response);
-			    console.log("BODY\n" + body);
-			    callback(body);
-			});
-	    }; 
-	})(func_name);
-
-
+        request(request_details,
+          function(error, response, body) {
+              console.log(request_details['method'] + " " + request_details['uri'] + "\n");
+              console.log("ERROR\n" + error);
+              console.log("RESPONSE\n" + response);
+              console.log("BODY\n" + body);
+              callback(body);
+        });
+      };
+  })(func_name);
 };
-
-
-
 
